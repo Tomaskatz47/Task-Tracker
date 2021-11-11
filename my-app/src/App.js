14a85964ff3d413678758cf69ace414cf3a07c97
Tomas Katz <tomykatz@gmail.com>
2021-11-10 21:12:40 -0300

    Finished addtask button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import Tasks from '../src/components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const[showAddTask, setShowAddTask] = useState(false)
   const[tasks, setTasks]= useState([
     {
       id: 1,
@@ -45,10 +46,10 @@ function App() {
     : task))                                 //else, stay the same
   }
 
-  return (
+  return (                                            //&& is an easy way to ternary conditional (if && then)
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}   
       {
         tasks.length > 0 ? (
           <Tasks tasks={tasks} onDelete= {deleteTask} onToggle={toggleReminder}/>)  
